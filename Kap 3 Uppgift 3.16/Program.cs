c2487fdb734b56524046759f6e88e4c5743e7125
Admin <Admin@TEB123021>
2022-10-05 12:12:01 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+namespace Kapitel3_uppguft3_16
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur länge vill du spela låten"); 
+            int svar = int.Parse (Console.ReadLine());
+
+            if (svar >= 240 && svar <= 420)
+            {
+                Console.WriteLine(" du får spela låten");
+            }
+
+            else 
+            {
+                Console.WriteLine(" Du får inte spela låten"); 
+
+            }
+            
+        }
+    }
+}
